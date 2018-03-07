847f0a5536dbae2e34ba9ee1700128920f0ea483
Krico16 <bash_stone_u@hotmail.com>
2018-03-06 20:51:20 -0500

    Pagina Rehecha

---
+++ Login/user.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <title>Titulo de la pagina</title>
+    <link rel="stylesheet" href="/Login/indexprops.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Amita' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Baloo' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Berkshire Swash' rel='stylesheet'>
+</head>
+
+<body>
+
+</body>
+
+</html>
